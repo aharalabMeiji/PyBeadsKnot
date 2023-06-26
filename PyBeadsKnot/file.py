2438053b0ed5ca261f4cefd690840907949f40ef
Kazushi Ahara <ahara@meiji.ac.jp>
2023-06-27 00:35:38 +0900

    Update file.py

--- PyBeadsKnot/file.py
+++ PyBeadsKnot/file.py
@@ -9,6 +9,26 @@ class fileIO:
 		pass
 
 	def loadBeadsKnotFile(self):
+		PrintWriter file; 
+		file = createWriter(file_name);
+		file.println("BeadsKnot,0");
+		file.println("Nodes,"+graph.nodes.size());
+		for (int nodeID=0; nodeID<graph.nodes.size(); nodeID++) {
+			Node nd = graph.nodes.get(nodeID);
+			if (nd.inUse) {
+				file.print(nd.x+","+nd.y+","+nd.theta+",");
+				file.println(nd.r[0]+","+nd.r[1]+","+nd.r[2]+","+nd.r[3]);
+			} else {
+				file.print(0+","+0+","+0+",");
+				file.println(10+","+10+","+10+","+10);
+			}
+		}
+		file.println("Edges,"+graph.edges.size());
+		for (int edgeID=0; edgeID<graph.edges.size(); edgeID++) {
+			Edge ed = graph.edges.get(edgeID);
+			file.println(ed.ANodeID+","+ed.ANodeRID+","+ed.BNodeID+","+ed.BNodeRID);
+		}
+
 		pass
 
 	def loadImageFile(self):
